3a561c23503f6309f63a8d474ed3702f49f2c96f
Rajdeep <snnbarot@gmail.com>
2024-01-10 21:06:44 +0530

    Refinement

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,7 @@
 <script>
   import Header from './pages/Hero.svelte'
   import ServicesSection from './pages/Services.svelte';
-  import Infographical from './pages/Infographical.svelte';
+  import Infographical from './pages/About.svelte';
   import Testimonials from './pages/Testimonials.svelte';
   import Navbar from './pages/Navbar.svelte';
   import Footer from './pages/Footer.svelte';
